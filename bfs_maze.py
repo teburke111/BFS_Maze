8e4b21130b707260595a57d3886d504577fd7b39
teburke111 <teburke111@gmail.com>
2025-02-21 10:15:49 -0500

    commit 2

--- bfs_maze.py
+++ bfs_maze.py
@@ -22,61 +22,68 @@ class Maze():
         print()
 
     def moves(self):
+        #set to empty list
         moves = []
+
+        #location
         y = self.location[0]
         x = self.location[1]
 
         if y - 1 >= 0 and self.grid[y-1][x] == ' ':
-            moves.append('U')
+            moves.append('U') # append Up
 
         if y + 1 < len(self.grid) and self.grid[y+1][x] == ' ':
-            moves.append('D')
+            moves.append('D') # append Down
 
         if x - 1 >= 0 and self.grid[y][x-1] == ' ':
-            moves.append('L')
+            moves.append('L') # append Left
 
         if x + 1 < len(self.grid) and self.grid[y][x+1] == ' ':
-            moves.append('R')
+            moves.append('R') # append Right
 
         return moves
         
 
     def neighbor(self, move):
+        #location
         y = self.location[0]
         x = self.location[1]
 
         if move == "U":
-            newLocation = (y-1,x)
+            newLocation = (y-1,x) # move Up
 
         if move == "D":
-            newLocation = (y+1,x)
+            newLocation = (y+1,x) # move Down
 
         if move == "L":
-            newLocation = (y,x-1)
+            newLocation = (y,x-1) # move Down
 
         if move == "R":
-            newLocation = (y,x+1)
+            newLocation = (y,x+1) # move Down
         
 
-        return Maze(self.grid, newLocation)
+        return Maze(self.grid, newLocation) # new maze with new location
 
 
 class Agent():
     def bfs(self, maze, goal):
+        # create frontier and visited node list
         frontier = deque([(maze.location, [])])
         visited = set()
 
+        #while frontier is not empty
         while frontier:
-            (current, path) = frontier.popleft()
+            (current, path) = frontier.popleft() #pop of current 
             # print(current,path)
             
 
-            if current == goal:
+            if current == goal: # check goal
                 return path  
             
-            visited.add(current)
+            visited.add(current) #add current to visited
 
-            current_maze = Maze(maze.grid, current)
+            current_maze = Maze(maze.grid, current) # new maze
+
             for move in current_maze.moves():
                 neighbor = current_maze.neighbor(move).location
                 
@@ -110,7 +117,7 @@ def main():
             "X X                X",
             "XXXXXXXXXXXXXXXXXX X"]
 
-    maze = Maze(grid,(1,1)) # start location
+    maze = Maze(grid,(1,1)) # set start location
     maze.display()
 
     agent = Agent()
